fab0071bab8c330e88af1104d7eec2f014a5e8d7
Marcus Hilaire <marcushilaire@gmail.com>
2018-05-13 19:51:07 -0400

    Class Constructors

--- ObjectOriented/oop.js
+++ ObjectOriented/oop.js
@@ -7,17 +7,33 @@ let objectTest = {
 };
 // objectTest.greet(objectTest.message);
 
-// constructor
+// constructors
 
-// ** this does not work with arrow functions
+// ** Cannot use arrow functions to define function declared constructor
 // let hardware = () => {
 // **
 
+// **  function declaration can be hoisted
+let gpu = new Hardware();
+// gpu.print("gtx 1080");
+// **
 function Hardware() {
   this.print = model => {
     console.log("this is a ", model);
     // console.log("hello");
   };
 }
-let gpu = new Hardware();
-gpu.print("gtx 1080");
+
+//class declarations cannot be hoisted
+
+class Animals {
+  constructor(species, breed) {
+    (this.species = species), (this.breed = breed);
+    this.pet = () => {
+      console.log("this is my", this.species, this.breed);
+    };
+  }
+}
+
+let puppy = new Animals("dog", "shiba");
+puppy.pet();
