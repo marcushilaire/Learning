14c2c45e2696043e51938f4dad868b2bd80304e8
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-12 12:34:25 -0400

    basis of testing with jest

--- UnitTesting/mageSpellCasting.js
+++ UnitTesting/mageSpellCasting.js
@@ -1,4 +1,4 @@
 // import { fireMage, FrostMage } from "Prototype.js";
-var mages = require("./Prototype.js");
+var mages = require("../ObjectOriented/Prototype.js");
 console.log(mages.fireMage.cast());
 console.log(mages.frostMage.cast());
---
+++ UnitTesting/mageSpellCasting.test.js
@@ -0,0 +1,1 @@
+var mages = require("../ObjectOriented/Prototype.js");
