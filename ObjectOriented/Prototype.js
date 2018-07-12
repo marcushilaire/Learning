5f7afde5d619d88081656946b0b77f03f40db0e1
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-12 12:24:19 -0400

    Exporting prototype.js

--- ObjectOriented/Prototype.js
+++ ObjectOriented/Prototype.js
@@ -33,19 +33,50 @@ Object.setPrototypeOf(cryptoBoom, nvidia);
 // Deeper into protoype
 // let mage = spell => {
 // arrow functions do not work in this case
-function Mage(spell, level) {
+function Mage(spell, spellPower, critChance) {
   this.spell = spell;
-  this.level = level;
+  this.critChance = critChance;
+  this.spellPower = spellPower;
+  this.spellMin = 100;
+  this.spellMax = 300;
 }
 
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+Mage.prototype.rollCritical = function(critChance) {
+  return critChance < randomNumber(0, 100);
+};
+
 Mage.prototype.cast = function() {
-  console.log(this);
-  console.log("Level", this.level, " Mage casting", this.spell);
+  console.log("Casting", this.spell);
+
+  // rolling damage of the spell
+  var baseDamage = randomNumber(this.spellMin, this.spellMax);
+  // console.log(baseDamage);
+
+  // scaling spell damage up
+  var scaleDamage = baseDamage * this.spellPower;
+  // console.log(scaleDamage);
+
+  // rolling to see if the spell crits
+  var critRoll = this.rollCritical(this.critChance);
+  // console.log(critRoll);
+
+  //final damage conditional
+  if (critRoll) {
+    return `Critical Hit! ${scaleDamage * 2}`;
+  }
+
+  return scaleDamage;
 };
 
 // These Objects do not contain the cast function when logged but have access to them
-var fireMage = new Mage("Pyroblast", 110); //{spell: "Pyroblast", level: 110}
-var frostMage = new Mage("Ice Lance", 90); //{spell: "Ice Lance, level: 90"}
+var fireMage = new Mage("Pyroblast", 15, 50); //{spell: "Pyroblast", level: 110}
+var frostMage = new Mage("Ice Lance", 20, 33); //{spell: "Ice Lance, level: 90"}
 
-fireMage.cast();
-frostMage.cast();
+// fireMage.cast();
+// frostMage.cast();
+
+module.exports = { fireMage, frostMage };
---
+++ ObjectOriented/moduleTest.js
@@ -0,0 +1,4 @@
+// import { fireMage, FrostMage } from "Prototype.js";
+var mages = require("./Prototype.js");
+console.log(mages.fireMage.cast());
+console.log(mages.frostMage.cast());
