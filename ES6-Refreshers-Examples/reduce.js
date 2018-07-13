1c2d564841675d91d3d7fd8baf7c37161d38377a
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-13 13:56:15 -0400

    Going through reduce documentation

---
+++ ES6-Refreshers-Examples/reduce.js
@@ -0,0 +1,18 @@
+const arr = [10, 11, 12, 13, 14];
+
+const sum = (accum, currVal, index, array) => {
+  console.log("Accumulator:", accum);
+  console.log("Current value sent to the callback:", currVal);
+  console.log("Index of the current Value:", index);
+  console.log("The array being reduced:", array);
+  console.log("======");
+  return accum + currVal;
+};
+// If nothing is returned from the callback function, reduce will return undefined
+
+console.log(arr.reduce(sum)); //60
+
+console.log(arr.reduce(sum, 100)); //160
+// If no accumulator(seen above as 100) is passed to reduce,
+//  the value at the 0th inex becomes the accumulator and
+//  and reduce begins at the 1st index
