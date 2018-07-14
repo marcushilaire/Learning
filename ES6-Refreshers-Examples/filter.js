11115e2542c6d3731267ce64866dadf2352b5d7e
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-14 12:16:46 -0400

    Expanding reduce and adding modularization

--- ES6-Refreshers-Examples/filter.js
+++ ES6-Refreshers-Examples/filter.js
@@ -1,42 +1,6 @@
-var arr = [
-  {
-    class: "Mage",
-    level: 110,
-    name: "Anthyous",
-    race: "Blood Elf",
-    faction: "Horde"
-  },
-  {
-    class: "Warrior",
-    level: 80,
-    name: "Heratio",
-    race: "Goblin",
-    faction: "Horde"
-  },
-  {
-    class: "Warlock",
-    level: 90,
-    name: "Johnothan",
-    race: "Human",
-    faction: "Alliance"
-  },
-  {
-    class: "Hunter",
-    level: 80,
-    name: "Joseph",
-    race: "Dwarf",
-    faction: "Alliance"
-  },
-  {
-    class: "Demonhunter",
-    level: 98,
-    name: "Ilidan",
-    race: "Nightelf",
-    faction: "Alliance"
-  }
-];
+const characters = require("./characters.json");
 
-let filterAlliance = (currentEle, index, array) => {
+const filterAlliance = (currentEle, index, array) => {
   console.log("Current Element sent to the Callback:", currentEle.name);
   console.log("Index of the current value", index);
   console.log("---");
@@ -46,6 +10,6 @@ let filterAlliance = (currentEle, index, array) => {
   return false;
 };
 
-console.log(arr.filter(filterAlliance));
+console.log(characters.filter(filterAlliance));
 
 // The amount of times filter runs is equal to arr.length
