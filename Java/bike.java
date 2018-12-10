ca15c4f9386fd4440a3bb212612f12c1fdda08c1
Marcus Hilaire <marcushilaire@gmail.com>
2018-12-10 17:42:53 -0400

    Making progress and succesfully running the files

--- Java/bike.java
+++ Java/bike.java
@@ -1,28 +1,27 @@
-public class Bicycle {
+class Bicycle {
 
     int cadence = 0;
     int speed = 0;
     int gear = 1;
 
     void changeCadence(int newValue) {
-         cadence = newValue;
+        cadence = newValue;
     }
 
     void changeGear(int newValue) {
-         gear = newValue;
+        gear = newValue;
     }
 
     void speedUp(int increment) {
-         speed = speed + increment;   
+        speed = speed + increment;
     }
 
     void applyBrakes(int decrement) {
-         speed = speed - decrement;
+        speed = speed - decrement;
     }
 
     void printStates() {
-         System.out.println("cadence:" +
-             cadence + " speed:" + 
-             speed + " gear:" + gear);
+        System.out.println("cadence:" + cadence + " speed:" + speed + " gear:" + gear);
     }
+
 }
