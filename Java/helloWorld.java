a0adb782668a2c845d20829504271c34e065b9a8
Marcus Hilaire <marcushilaire@gmail.com>
2018-12-10 18:13:49 -0400

    further understanding of the main method

--- Java/helloWorld.java
+++ Java/helloWorld.java
@@ -1,5 +1,16 @@
 class HelloWorldApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) {// the main method
+        // this is where all methods in the application will be invoked
+
+        // String[] args this indicates that the one argument being passed is an array
+        // of strings
+
+        // They are command line arguments just like the process.argv array in
+        // javascript.
+
         System.out.println("Hello World!"); // Display the string.
+        for (String arg : args) {
+            System.out.println(arg);
+        } // forloop to print all command line arguments
     }
 }
