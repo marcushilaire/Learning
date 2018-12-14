c9a83ca784385cfe32639225d27b7053c4a452a4
Marcus Hilaire <marcushilaire@gmail.com>
2018-12-13 22:19:11 -0400

    more packaging

--- Java/Tutorial/mygame/server/Server.java
+++ Java/Tutorial/mygame/server/Server.java
@@ -8,28 +8,29 @@ import mygame.shared.Utilities;
 
 public class Server {
 
-    public static void main(String args[]) {
-	ServerSocket serverSocket = null;
+	public static void main(String args[]) {
+		ServerSocket serverSocket = null;
 
-	Utilities.printMsg("creating server socket");
-	
-	try {
-	    serverSocket = new ServerSocket(4444);
-	} catch (IOException e) {
-	    System.err.println("Unable to create server socket, " + e);
-	    System.exit(1);
+		Utilities.printMsg("creating server socket");
+
+		try {
+			serverSocket = new ServerSocket(4444);
+		} catch (IOException e) {
+			System.err.println("Unable to create server socket, " + e);
+			System.exit(1);
+		}
+
+		Utilities.printMsg("accepting client connections");
+
+		while (true) {
+			try {
+				Socket clientSocket = serverSocket.accept();
+				new Client(clientSocket).start();
+				Utilities.printMsg("server running?");
+			} catch (IOException e) {
+				System.err.println("Unable to accept socket connection, " + e);
+				System.exit(1);
+			}
+		}
 	}
-
-	Utilities.printMsg("accepting client connections");
-
-	while (true) {
-	    try {
-		Socket clientSocket = serverSocket.accept();
-		new Client(clientSocket).start();
-	    } catch (IOException e) {
-		System.err.println("Unable to accept socket connection, " + e); 
-		System.exit(1);
-	    }
-	}
-    }
 }
