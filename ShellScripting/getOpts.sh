2a5ce3ad99d54260f6bf3fe632fe735c47b27e5c
Marcus Hilaire <marcushilaire@gmail.com>
2019-04-14 18:11:48 -0400

    Adjusting to fit my own example

--- ShellScripting/getOpts.sh
+++ ShellScripting/getOpts.sh
@@ -1,21 +1,19 @@
-while getopts 'lha:' OPTION; do
-  case "$OPTION" in
-    l)
-      echo "linuxconfig"
-      ;;
+#!/bin/bash
+handleArgs(){
+    while getopts 'ehxuz' OPTION; 
+    do case $OPTION in
+        e) echo option selected e;;
+        h) echo option selected h;;
+        x) echo option selected x;;
+        u) echo option selected u;;
+        z) echo option selected h ;;
+        ?) echo "script usage: $(basename $0) [-l] [-h] [-a somevalue]" 
+    esac
+    done
+    shift "$(($OPTIND -1))"
+}
 
-    h)
-      echo "h stands for h"
-      ;;
-
-    a)
-      avalue="$OPTARG"
-      echo "The value provided is $OPTARG"
-      ;;
-    ?)
-      echo "script usage: $(basename $0) [-l] [-h] [-a somevalue]" >&2
-      exit 1
-      ;;
-  esac
-done
-shift "$(($OPTIND -1))"
+handleArgs $@ 
+#handleArgs must be called on $@ in order to parse through the argument list
+#it ignores all non flagged arguments
+#will work with -ehx or -eh -x
