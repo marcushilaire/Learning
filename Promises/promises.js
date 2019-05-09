a347be5aa081ee573de13dc8226fa6131aa36099
Marcus Hilaire <marcushilaire@gmail.com>
2019-05-09 10:45:42 -0400

    Reading from blue oyster cult

--- Promises/promises.js
+++ Promises/promises.js
@@ -3,10 +3,7 @@ const fs = require("fs");
 const readLyrics = (file, encoding) => {
   return new Promise((resolve, reject) => {
     const myCallback = (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+      if (err) reject(`Error code: ${err.code}`); //essentialy treated as throwing an error
       resolve(data);
     };
     fs.readFile(file, encoding, myCallback);
@@ -16,5 +13,9 @@ const readLyrics = (file, encoding) => {
 readLyrics("../Helpers/Flyte.txt", "utf8").then(data => {
   console.log(data);
 });
+// .catch(err => {
+//   console.log(err);
+// });
+
 //the promise constructor takes in one argument, a function.
 // that function takes two functions a arguments, resolve  and reject
